8817e02dc6880dd2439304db1f79c82ecaa2c208
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-13 12:06:11 +0530

    roboshop-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -13,11 +13,11 @@ yum install rabbitmq-server -y &>>$log_file
 validate $?
 
 head "enabling "
-systemctl enable rabbitmq  &>>$log_file
+systemctl enable rabbitmq-server   &>>$log_file
 validate $?
 
 head "starting"
-systemctl start rabbitmq &>>$log_file
+systemctl start rabbitmq-server  &>>$log_file
 validate $?
 
 rabbitmqctl add_user roboshop roboshop123 &>>$log_file
