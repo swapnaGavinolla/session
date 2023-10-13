16d43be5c8eb86983b83655cb6906874328baac6
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-13 11:38:07 +0530

    roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -61,7 +61,7 @@ yum install mongodb-org-shell -y &>>$log_file
 validate $?
 
 head "loading schema"
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js &>>$log_file
+mongo --host 172.31.22.208 < /app/schema/catalogue.js &>>$log_file
 validate $?
 
 
